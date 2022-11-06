a1ff7b04d655c60eb042455804fa772f2f78a343
Hyeongwoo Jang <117496650+woooddyy@users.noreply.github.com>
2022-11-06 13:18:37 +0900

    Update conn.php
    비밀 정보 보호

--- fila/sub/conn.php
+++ fila/sub/conn.php
@@ -1,7 +1,7 @@
 <?php
 	header("content-type:text/html; charset=utf-8");
 
-	$conn=mysqli_connect("localhost","hyeongwoo","guddn1312130!#","hyeongwoo");
+	$conn=mysqli_connect("localhost","**","**","**");
 	if($conn->connect_error){
 		echo $conn->connect_errorno;
 		exit;
